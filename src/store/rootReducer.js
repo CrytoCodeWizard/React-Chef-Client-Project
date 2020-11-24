131510fb0fa0acf9c8d879f5cc526237da6168ad
David Teo <starrynightskyhoney@gmail.com>
2020-11-24 15:04:24 +0100

    Add users Reducer to save all users on the server

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import appState from "./appState/appStateReducer";
 import userLogin from "./userLogin/userLoginReducer";
+import users from "./users/userReducer";
 
 export default combineReducers({
   appState,
   userLogin,
+  users,
 });
