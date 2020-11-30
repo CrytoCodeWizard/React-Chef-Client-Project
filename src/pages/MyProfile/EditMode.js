e53dcf0c4c60492689fda82bc47573c8b29cccab
David Teo <starrynightskyhoney@gmail.com>
2020-11-30 15:59:51 +0100

    Refactored editmode with bootstrap styles

--- src/pages/MyProfile/EditMode.js
+++ src/pages/MyProfile/EditMode.js
@@ -1,41 +1,71 @@
 import React from "react";
+import { FormControl, InputGroup } from "react-bootstrap";
+
 import "./MyProfile.css";
 
 function EditMode({ setEditProfile, chef, editProfile }) {
   return (
     <div className="MyProfile-editMode-wrapper">
       <h5>Edit profile details...</h5>
-      <input
-        onChange={(e) => setEditProfile({ ...editProfile, yearsOfExperience: e.target.value })}
-        className="MyProfile-editMode-input"
-        defaultValue={editProfile.yearsOfExperience}
-        type="text"
-      />
-      <input
-        onChange={(e) => setEditProfile({ ...editProfile, hourlyRate: e.target.value })}
-        defaultValue={editProfile.hourlyRate}
-        className="MyProfile-editMode-input"
-        type="text"
-      />
-      <input
-        onChange={(e) => setEditProfile({ ...editProfile, position: e.target.value })}
-        className="MyProfile-editMode-input"
-        defaultValue={editProfile.position}
-        type="text"
-      />
-      <input
-        onChange={(e) => setEditProfile({ ...editProfile, city: e.target.value })}
-        className="MyProfile-editMode-input"
-        defaultValue={editProfile.city}
-        type="text"
-      />
-      <textarea
-        className="MyProfile-editMode-textarea"
-        onChange={(e) => setEditProfile({ ...editProfile, description: e.target.value })}
-        rows="6"
-        cols="30"
-        defaultValue={chef && chef.profile.description}
-      ></textarea>
+
+      <InputGroup className="mb-1">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            <i className="las la-user-cog"></i>
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          onChange={(e) => setEditProfile({ ...editProfile, yearsOfExperience: e.target.value })}
+          defaultValue={editProfile.yearsOfExperience}
+          aria-label="experience"
+        />
+      </InputGroup>
+      <InputGroup className="mb-1">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            <i className="las la-euro-sign"></i>
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          onChange={(e) => setEditProfile({ ...editProfile, hourlyRate: e.target.value })}
+          defaultValue={editProfile.hourlyRate}
+          aria-label="rate"
+        />
+      </InputGroup>
+      <InputGroup className="mb-1">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            <i className="las la-user-tag"></i>
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          onChange={(e) => setEditProfile({ ...editProfile, position: e.target.value })}
+          defaultValue={editProfile.position}
+          aria-label="rate"
+        />
+      </InputGroup>
+      <InputGroup className="mb-1">
+        <InputGroup.Prepend>
+          <InputGroup.Text>
+            <i className="las la-map-marker-alt"></i>
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          onChange={(e) => setEditProfile({ ...editProfile, city: e.target.value })}
+          defaultValue={editProfile.city}
+          aria-label="rate"
+        />
+      </InputGroup>
+
+      <InputGroup>
+        <FormControl
+          onChange={(e) => setEditProfile({ ...editProfile, description: e.target.value })}
+          defaultValue={chef && chef.profile.description}
+          as="textarea"
+          aria-label="description"
+          rows={6}
+        />
+      </InputGroup>
     </div>
   );
 }
