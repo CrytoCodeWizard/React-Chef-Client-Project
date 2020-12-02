0805cf57061284506f33f90772d53bc7a648ff74
David Teo <starrynightskyhoney@gmail.com>
2020-12-02 12:41:39 +0100

    Add tooltip to availability toggler

--- src/components/Calendar/Day.js
+++ src/components/Calendar/Day.js
@@ -2,27 +2,33 @@ import React from "react";
 import dayStyles, { availableStyles } from "./styles.js";
 import { addAvailableDate, removeAvailableDate } from "../../store/users/userActions";
 import { useDispatch } from "react-redux";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const Day = (props) => {
   const { dayIndex, same, day, selectedDate } = props;
-
   const dispatch = useDispatch();
   return (
-    <div
-      key={dayIndex}
-      style={availableStyles(same)}
-      className="day"
-      onClick={() => {
-        const availableDate = day.format("YYYY-MM-DD");
-        if (same) {
-          dispatch(removeAvailableDate(availableDate));
-        } else {
-          dispatch(addAvailableDate(availableDate));
-        }
-      }}
+    <OverlayTrigger
+      delay={{ show: 300, hide: 100 }}
+      placement={"bottom"}
+      overlay={<Tooltip>Toggle availability</Tooltip>}
     >
-      <div className={dayStyles(day, selectedDate)}>{day.format("D")}</div>
-    </div>
+      <div
+        key={dayIndex}
+        style={availableStyles(same)}
+        className="day"
+        onClick={() => {
+          const availableDate = day.format("YYYY-MM-DD");
+          if (same) {
+            dispatch(removeAvailableDate(availableDate));
+          } else {
+            dispatch(addAvailableDate(availableDate));
+          }
+        }}
+      >
+        <div className={dayStyles(day, selectedDate)}>{day.format("D")}</div>
+      </div>
+    </OverlayTrigger>
   );
 };
 
